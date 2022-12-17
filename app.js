5803f3afd995d4f84e6d6e0dfd563d23409f1cce
roopesh <vetcharoopesh@gmail.com>
2022-12-17 15:41:46 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+const body = document.getElementById('bodyMain')
+const switchMain = document.getElementById('modeSwitcherBefore')
+const resumeButton = document.querySelector('.resume')
+function changeLight(){
+    body.classList.toggle('light-mode')
+    switchMain.classList.toggle('modeSwitcherAfter')
+    resumeButton.style.hover.backgroundColor = 'red'    
+};
+
+
+// const shower = document.getElementById('moreAboutMe');
+// const myBiograph = document.getElementById('aboutMeBox')
+
+
+// shower.addEventListener('click',function(){
+//     myBiograph.classList.toggle('glass-socialmedia-icons-after');
+//     myBiograph.style.transition = 'all 0.5s'
+// });
